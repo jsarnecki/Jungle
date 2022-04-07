fa82c26f5373e4c813099022168439a8ee18c1bd
Josh Sarnecki <joshsarnecki@gmail.com>
2022-04-07 17:42:54 +0000

    Created the basic functionality for the admin categories section.  Admin can now add new categories to the list of categories, and see a list of just the names of the categories in the index page.  Tested, with the ability to add these new categories to new products if desired.  Categories only require a name as of now, may implement adding a description later on, though would have to alter the original database schema

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,34 @@
 class Admin::CategoriesController < ApplicationController
+
+  admin_username = ENV["ADMIN_USERNAME"]
+  admin_password = ENV["ADMIN_PASSWORD"]
+
+  http_basic_authenticate_with name: admin_username, password: admin_password  
+
   def index
+    @category = Category.all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(product_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
+
+  private
+
+  def product_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:delete, :show, :edit, :update, :destroy]
+    resources :categories, except: [:show, :edit, :update, :destroy]
   end
 
 
