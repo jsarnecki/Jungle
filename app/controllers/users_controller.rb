111ceb8d8ebbff4fa35cc158d5b2034217e29280
Josh Sarnecki <joshsarnecki@gmail.com>
2022-04-07 20:36:47 +0000

    Rollbacked the migration to add in the last_name param, adding it to the signup form.  Tested, works.  Altered the signin form to have same table setup as the signup form.  Tested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,14 +12,10 @@ class UsersController < ApplicationController
     end
   end
 
-  def signed_in_title
-    @signed_in_title = "Signed in as #{current_user.name}"
-  end
-
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end
