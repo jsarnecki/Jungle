ff8d66a508349a42b2ce0d2361b2cb7e9532c121
Josh Sarnecki <joshsarnecki@gmail.com>
2022-04-11 21:47:21 +0000

    Began writing validation specs for Product

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    
+    it 'validates that the product has a :name' do
+      @category = Category.create(:name => 'test')
+      @product = Product.create(:category_id => @category.id, :name => 'test_product')
+      expect(@product.name).to be_present
+    end
+
+    it 'validates that the product has a :price' do
+
+    end
+
+    it 'validates that the product has a :quantity' do
+    
+    end
+
+    it 'validates that the product has a :category' do
+    
+    end
+
+  end
+end
