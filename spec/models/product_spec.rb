7ad65bc6e6325ba6623bc97c9c561fefa985ddb9
Josh Sarnecki <joshsarnecki@gmail.com>
2022-04-11 21:57:26 +0000

    More tests with RSpec on Product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+
+    it 'validates that when all four fields are set, it saves successfully' do
+      @category = Category.create(:name => 'test')
+      @product = Product.create(:category_id => @category.id, :name => 'test_product', :price => 100, :quantity => 5)
+      expect(@product).to be_present
+    end
     
     it 'validates that the product has a :name' do
       @category = Category.create(:name => 'test')
@@ -11,15 +17,16 @@ RSpec.describe Product, type: :model do
 
     it 'validates that the product has a :price' do
       @category = Category.create(:name => 'test')
-      @product = Product.create(:category_id => @category.id)
-
+      @product = Product.create(:category_id => @category.id, :price => 5000)
+      expect(@product.price).to be_present
+      
     end
 
-    it 'validates that the product has a :quantity' do
+    xit 'validates that the product has a :quantity' do
     
     end
 
-    it 'validates that the product has a :category' do
+    xit 'validates that the product has a :category' do
     
     end
 
