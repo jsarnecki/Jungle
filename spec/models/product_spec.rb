edf6e5e4ecfaedab3d5f2529576ac6a6bcae5c99
Josh Sarnecki <joshsarnecki@gmail.com>
2022-04-11 22:34:07 +0000

    Finished with validation tests for Product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,28 +6,34 @@ RSpec.describe Product, type: :model do
     it 'validates that when all four fields are set, it saves successfully' do
       @category = Category.create(:name => 'test')
       @product = Product.create(:category_id => @category.id, :name => 'test_product', :price => 100, :quantity => 5)
-      expect(@product).to be_present
-    end
-    
-    it 'validates that the product has a :name' do
-      @category = Category.create(:name => 'test')
-      @product = Product.create(:category_id => @category.id, :name => 'test_product')
-      expect(@product.name).to be_present
-    end
-
-    it 'validates that the product has a :price' do
-      @category = Category.create(:name => 'test')
-      @product = Product.create(:category_id => @category.id, :price => 5000)
+      expect(@product.category_id).to be_present
       expect(@product.price).to be_present
-      
+      expect(@product.name).to be_present
+      expect(@product.quantity).to be_present
+    end
+    
+    it 'validates that the product must contain :name' do
+      @category = Category.create(:name => 'test')
+      @product = Product.create(:category_id => @category.id, :quantity => 10, :price => 5000, :name => nil)
+      expect(@product.errors.full_messages).to include "Name can't be blank"
     end
 
-    xit 'validates that the product has a :quantity' do
-    
+    it 'validates that the product must contain :price' do
+      @category = Category.create(:name => 'test')
+      @product = Product.create(:category_id => @category.id, :quantity => 10, :name => 'test_product', :price => nil)
+      expect(@product.errors.full_messages).to include "Price can't be blank"
     end
 
-    xit 'validates that the product has a :category' do
-    
+    it 'validates that the product must contain :quantity' do
+      @category = Category.create(:name => 'test')
+      @product = Product.create(:category_id => @category.id, :quantity => nil, :price => 5000, :name => 'text_product')
+      expect(@product.errors.full_messages).to include "Quantity can't be blank"
+    end
+
+    it 'validates that the product must contain :category' do
+      @category = Category.create(:name => 'test')
+      @product = Product.create(:category_id => nil, :quantity => 10, :price => 5000, :name => 'text_product')
+      expect(@product.errors.full_messages).to include "Category can't be blank"
     end
 
   end
