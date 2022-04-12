dd7e60f35c5ce95d405a7d041a0c0b9fa0870370
Josh Sarnecki <joshsarnecki@gmail.com>
2022-04-12 01:14:02 +0000

    Created better authentication in sessionsController for email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  def authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user.authenticate(password)
+      user
+    else
+      nil
+    end
   
 end
