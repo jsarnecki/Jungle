4bf4d2501dc93d0d2243fee45be26660be772788
Josh Sarnecki <joshsarnecki@gmail.com>
2022-04-12 02:58:11 +0000

    More RSpec testing on User model, though fairly certain not using best practices

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,11 @@ class User < ActiveRecord::Base
     else
       nil
     end
+  end
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, length: {minimum: 5}
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,22 +3,46 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Validations" do
 
-    it "must be created with password and password_confirm" do
+    xit "must be created with password and password_confirm" do
+      @user = User.create(
+        :first_name => "test", 
+        :last_name => "123", 
+        :email => "test@123", 
+        :password => "12345",
+        :password_confirmation => "12345")
+
+      expect(@user.password == @user.password_confirmation).to be_true
 
     end
 
     it "must be created with a unique email" do
+      @user = User.new(
+        :first_name => "test", 
+        :last_name => "123", 
+        :email => "test@123", 
+        :password => "12345",
+        :password_confirmation => "12345")
 
+      email_confirm = User.find_by_email(@user.email)
 
-
+      expect(email_confirm).to be_nil
     end
 
     it "must include a first and last name" do
-
-
+      @user = User.create(:first_name => "Josh", :last_name => "Man", :email => "test@test")
+      expect(@user.first_name).to be_present
+      expect(@user.last_name).to be_present
     end
 
-    it "must have a password with a minimum length of....X" do
+    it "must have a password with a minimum length of 5" do
+      @user = User.create(
+        :first_name => "Test", 
+        :last_name => "123", 
+        :email => "test@123", 
+        :password => "1234",
+        :password_confirmation => "1234")
+      
+        expect(@user.errors.full_messages).to include "Password is too short (minimum is 5 characters)"
     end
 
   end
