2864075e73c4417bfaffba67b72646ece1cfe003
Josh Sarnecki <joshsarnecki@gmail.com>
2022-04-12 01:00:13 +0000

    Setting up RSpecs for User

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "Validations" do
+
+    it "must be created with password and password_confirm" do
+
+    end
+
+    it "must be created with a unique email" do
+
+
+
+    end
+
+    it "must include a first and last name" do
+
+
+    end
+
+    it "must have a password with a minimum length of....X" do
+    end
+
+  end
+
+  describe ".authenticate_with_credentials" do
+
+  end
+end
