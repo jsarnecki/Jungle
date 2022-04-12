1e82776679566668238864a993a3a069816265ea
Josh Sarnecki <joshsarnecki@gmail.com>
2022-04-12 16:21:03 +0000

    Finished writing RSpec tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,6 @@ RSpec.describe User, type: :model do
     end
 
     it "returns invalid when password and confirmation does not match" do
-
       @user = User.create(
         :first_name => "test", 
         :last_name => "123", 
@@ -28,7 +27,7 @@ RSpec.describe User, type: :model do
 
     end
 
-    xit "must be created with a unique email" do
+    it "must be created with a unique email" do
       @user = User.new(
         :first_name => "test", 
         :last_name => "123", 
@@ -41,16 +40,29 @@ RSpec.describe User, type: :model do
       expect(email_confirm).to be_nil
     end
 
-    xit "must include a first and last name" do
-      @user = User.create(:first_name => "Josh", :last_name => "Man", :email => "test@test")
-
-      expect(@user.valid?).to be_truthy
+    it "must include a first and last name" do
+      @user = User.create(
+        :first_name => "Test", 
+        :last_name => "123", 
+        :email => "test@123", 
+        :password => "12345",
+        :password_confirmation => "12345")
+      
       expect(@user.valid?).to be_truthy
     end
 
-    #test functionality > value
+    it "will return invalid if missing a first or last name" do
+      @user = User.create(
+        :first_name => "Test", 
+        :last_name => nil, 
+        :email => "test@123", 
+        :password => "12345",
+        :password_confirmation => "12345")
+      
+      expect(@user.valid?).to be_falsey
+    end
 
-    xit "must have a password with a minimum length of 5" do
+    it "must have a error message when password length is less than 5" do
       @user = User.create(
         :first_name => "Test", 
         :last_name => "123", 
@@ -59,6 +71,17 @@ RSpec.describe User, type: :model do
         :password_confirmation => "1234")
       
         expect(@user.errors.full_messages).to include "Password is too short (minimum is 5 characters)"
+    end
+
+    it "must have a password with minimum length of 5" do
+      @user = User.create(
+        :first_name => "Test", 
+        :last_name => "123", 
+        :email => "test@123", 
+        :password => "12345",
+        :password_confirmation => "12345")
+      
+        expect(@user.valid?).to be_truthy 
     end
 
   end
@@ -80,12 +103,10 @@ RSpec.describe User, type: :model do
     end
 
     it "should still login users if their email spelling is correct, but case is wrong" do
-
       email = "Test@123"
       expect(@user.authenticate_with_credentials(email, "12345")).to be_truthy
 
     end
-
 
   end
 end
