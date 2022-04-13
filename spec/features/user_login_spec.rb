9bd8cabe2fe2083b6a782ea356a5379339fdbe67
Josh Sarnecki <joshsarnecki@gmail.com>
2022-04-13 22:29:03 +0000

    Scrapped the original approach, now starting on the login page, able to fill in the appropriate inputs

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -14,13 +14,19 @@ RSpec.feature "UserLogins", type: :feature, js: true do
 
 scenario "They can successfully login and are taken to the home page" do
 
-  visit root_path
+  visit "/login"
 
   #find the login form
+  # page.find('.navbar-right').find_link('Login').click
   #fill_in the form with above credentials, click login
+  expect(page).to have_text 'Login'
+  # puts page.html
+  fill_in "email", with: "test@123"
+  fill_in "password", with: "12345"
+
   #verify on home page and there is logout button present
 
-  save_screenshot
+  # save_screenshot
 
 end
 
